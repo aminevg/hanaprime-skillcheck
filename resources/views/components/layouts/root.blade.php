df3889965dcd772eee7f4f70e67d5b3137949420
aminevg <38422328+aminevg@users.noreply.github.com>
2025-01-17 08:47:08 +0000

    Fix head title

--- resources/views/components/layouts/root.blade.php
+++ resources/views/components/layouts/root.blade.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{ $title ? $title . '- 1行日記' : '1行日記' }}</title>
+    <title>{{ $title ? $title . ' - 1行日記' : '1行日記' }}</title>
 
     <!-- Styles / Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
