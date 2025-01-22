3e09d1335ae6a86f205379fd3f3dbef3516bb675
aminevg <38422328+aminevg@users.noreply.github.com>
2025-01-22 08:26:22 +0000

    Change diary edit label

--- resources/views/diaries/edit.blade.php
+++ resources/views/diaries/edit.blade.php
@@ -1,4 +1,4 @@
 <x-layouts.authenticated title="日記を編集">
-    <x-diaries.form title="日記を編集" label="編集" :action="route('diaries.store')" method="patch"
+    <x-diaries.form title="日記を編集" label="保存" :action="route('diaries.store')" method="patch"
         :diary="$diary" />
 </x-layouts.authenticated>
